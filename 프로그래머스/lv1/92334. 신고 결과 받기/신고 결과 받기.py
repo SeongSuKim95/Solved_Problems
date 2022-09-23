c4bca24f300e0e432a5facc31cc8f0b396d07a16
Kim Seong Su <62092317+SeongSuKim95@users.noreply.github.com>
2022-09-23 16:14:17 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -0,0 +1,16 @@
+
+def solution(id_list, report, k):
+    answer = [0] * len(id_list)    
+    reports = {x : 0 for x in id_list}
+
+    for r in set(report):
+        reports[r.split()[1]] += 1
+
+    for r in set(report):
+        if reports[r.split()[1]] >= k:
+            
+            answer[id_list.index(r.split()[0])] += 1
+    
+    list_s  = [1,2,34]
+    print(list_s.index(34))
+    return answer
