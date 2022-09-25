7f8ae8e9c182c88fc293886e6dfd7f38c9f5c423
Kim Seong Su <62092317+SeongSuKim95@users.noreply.github.com>
2022-09-25 20:29:57 +0900

    [Gold IV] Title: 테트로미노, Time: 256 ms, Memory: 37316 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -1,56 +1,44 @@
 import sys; input = sys.stdin.readline
 
-def dfs(r, c, idx, total):
-    global ans
-    if idx == 3:
-        if total > ans:
-            ans = total
-    else:
-        for i in range(4):
-            nr = r + dr[i]
-            nc = c + dc[i]
-            if 0 <= nr < N and 0 <= nc < M:
-                if visit[nr][nc] == 0:
-                    visit[nr][nc] = 1
-                    dfs(nr, nc, idx + 1, total + arr[nr][nc])
-                    visit[nr][nc] = 0
+def dfs(x, y, step, total):
+    global answer
+    # 종료조건1) 탐색을 계속 진행하여도 최댓값에 못 미치는 경우
+    if total + max_val*(4-step) <= answer:
+        return
 
-def block(r, c, total):
-    global ans
-    make_block = 0
-    for i in range(4):
-        nr = r + dr[i]
-        nc = c + dc[i]
-        if 0 <= nr < N and 0 <= nc < M:
-            make_block += 1
-            total += arr[nr][nc]
+    # 종료조건2) 블록 4개를 모두 활용한 경우
+    if step == 4:
+        answer = max(answer, total)
+        return
 
-    if make_block == 3:
-        if total > ans:
-            ans = total
+    # 상하좌우 방향으로 블록 이어 붙여 나가기
+    for dx, dy in d:
+        nx = x + dx # 새로운 x 좌표
+        ny = y + dy # 새로운 y 좌표
+        # 새로운 좌표가 유효한 범위 내 있고 탐색이력이 없는 경우
+        if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny]:
+            # 2번째 블록 연결 후 'ㅏ','ㅓ','ㅗ','ㅜ' 만들기
+            if step == 2:
+                visited[nx][ny] = True # 탐색기록
+                # 새로운 좌표에서 탐색하지 않고 기존 좌표로 돌아와 탐색재개
+                dfs(x, y, step+1, total+board[nx][ny])
+                visited[nx][ny] = False # 탐색기록 제거
 
-    if make_block == 4:
-        for i in range(4):
-            nr = r + dr[i]
-            nc = c + dc[i]
-            total -= arr[nr][nc]
-            if total > ans:
-                ans = total
-            total += arr[nr][nc]
+            visited[nx][ny] = True
+            dfs(nx, ny, step+1, total+board[nx][ny])
+            visited[nx][ny] = False
 
+if __name__ == "__main__":
+    N, M = map(int, input().split()) # 좌표의 행, 열 개수
+    board = [list(map(int, input().split())) for _ in range(N)] # 좌표별 값
+    max_val = max(map(max, board)) # 모든 좌표 중 최댓값
+    d = [(-1, 0), (1, 0), (0, -1), (0, 1)] # 좌표 내 상하좌우
+    visited = [[False] * M for _ in range(N)] # 탐색여부 확인용
+    answer = 0
 
-N, M = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(N)]
-visit = [([0] * M) for _ in range(N)]
-dr = [-1, 0, 1, 0]
-dc = [0, 1, 0, -1]
-ans = 0
-
-for r in range(N):
-    for c in range(M):
-        visit[r][c] = 1
-        dfs(r, c, 0, arr[r][c])
-        block(r, c, arr[r][c])
-        visit[r][c] = 0
-
-print(ans)
+    for i in range(N):
+        for j in range(M):
+            visited[i][j] = True # 탐색기록
+            dfs(i, j, 1, board[i][j])
+            visited[i][j] = False # 탐색기록 제거
+    print(answer)
