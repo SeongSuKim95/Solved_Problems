a74067c094bc74975bb8e01a070e58d20ab1eedf
Kim Seong Su <62092317+SeongSuKim95@users.noreply.github.com>
2022-10-02 23:02:01 +0900

    [Gold V] Title: 톱니바퀴, Time: 92 ms, Memory: 32460 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
@@ -1,38 +1,38 @@
 from collections import deque
 
-circle = [deque(map(int,input())) for _ in range(4)]
-opers = deque(list(map(int,input().split())) for _ in range(int(input())))
 
-while opers:	# 명령어가 남아 있으면 반복문 실행
-    num,direct = opers.popleft()
-    num-=1  # 0 index
-    tmp_2 = circle[num][2]	# 비교 대상 백업
-    tmp_6 = circle[num][6]	# 비교 대상 백업
-    circle[num].rotate(direct)	# 일단 시작 톱니 돌려주기
-    tmp_direct=direct
-    
-    # 시작 톱니 왼쪽 방향
-    direct=tmp_direct
-    for i in range(num-1,0-1,-1):
-        if circle[i][2]!=tmp_6:		# 서로 다른 극인 경우
-            tmp_6=circle[i][6]
-            circle[i].rotate(direct*-1)
-            direct*=-1
-        else:
-            break
+def rotate_right(x, d):
+    if x > 4 or gears[x - 1][2] == gears[x][6]:
+        return
 
-    # 시작 톱니 오른쪽 방향
-    direct=tmp_direct
-    for i in range(num+1,4):
-        if circle[i][6]!=tmp_2:		# 서로 다른 극인 경우
-            tmp_2 = circle[i][2]
-            circle[i].rotate(direct*-1)
-            direct*=-1
-        else:
-            break
+    if gears[x - 1][2] != gears[x][6]:
+        rotate_right(x + 1, -d)
+        gears[x].rotate(d)
 
-ans=0
+
+def rotate_left(x, d):
+    if x < 1 or gears[x][2] == gears[x + 1][6]:
+        return
+
+    if gears[x][2] != gears[x + 1][6]:
+        rotate_left(x - 1, -d)
+        gears[x].rotate(d)
+
+
+gears = {}
+for i in range(1, 5):
+    gears[i] = deque((map(int, input())))
+
+for _ in range(int(input())):
+    x, d = map(int, input().split())
+
+    rotate_right(x + 1, -d)
+    rotate_left(x - 1, -d)
+    gears[x].rotate(d)
+
+
+ans = 0
 for i in range(4):
-    if circle[i][0]==1:
-        ans += (2**i)
+    ans += gears[i + 1][0] * (2 ** i)
+
 print(ans)
