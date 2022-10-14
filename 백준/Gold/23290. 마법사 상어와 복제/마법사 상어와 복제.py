83ec63403a986b826a6a4f4312feb85888284cc1
Kim Seong Su <62092317+SeongSuKim95@users.noreply.github.com>
2022-10-14 17:43:00 +0900

    [Gold I] Title: 마법사 상어와 복제, Time: 2728 ms, Memory: 46612 KB -BaekjoonHub

--- 백준/Gold/23290. 마법사 상어와 복제/마법사 상어와 복제.py
+++ 백준/Gold/23290. 마법사 상어와 복제/마법사 상어와 복제.py
@@ -1,94 +1,90 @@
 import copy
 
 def move_fish():
-    """
-    물고기 이동
-    1. 상어가 있는 칸, 물고기 냄새 칸, 벗어나는 칸 x 
-    2. 45도 반시계 회전 후 이동. 이동 못하는 경우 그대로 
-    :return:
-    """
     res = [[[] for _ in range(4)] for _ in range(4)]
-    for x in range(4):
-        for y in range(4):
-            while temp[x][y]:
-                d = temp[x][y].pop()
-                for i in range(d, d - 8, -1):
+
+    for row in range(4):
+        for col in range(4):
+            while temp[row][col]:
+                cur_d = temp[row][col].pop()
+                for i in range(cur_d,cur_d-8,-1): # 반시계 방향
                     i %= 8
-                    nx, ny = x + f_dx[i], y + f_dy[i]
-                    if (nx, ny) != shark and 0 <= nx < 4 and 0 <= ny < 4 and not smell[nx][ny]:
-                        res[nx][ny].append(i)
+                    nrow, ncol = row + f_dx[i], col + f_dy[i]
+                    if 0<= nrow < 4 and 0<= ncol <4 and (nrow,ncol) != shark and not smell[nrow][ncol]:
+                        res[nrow][ncol].append(i)
                         break
                 else:
-                    res[x][y].append(d)
+                    res[row][col].append(cur_d)
     return res
 
 def dfs(x, y, dep, cnt, visit):
-    """
-    상어 3칸 이동
-    1. 제외되는 물고기 수가 많고 > 이동방법 사전순(백트래킹하면 자동으로 됨) 
-    2. 이동한 곳을 저장 > 물고기 냄새가 됨  
-    """
+
     global max_eat, shark, eat
-    if dep == 3:   # 3번 이동한 경우 그만 
+
+    if dep == 3:
         if max_eat < cnt:
             max_eat = cnt
-            shark = (x, y)
+            shark = (x,y)
             eat = visit[:]
         return
+    
     for d in range(4):
         nx = x + dx[d]
         ny = y + dy[d]
-        if 0 <= nx < 4 and 0 <= ny < 4:
-            if (nx, ny) not in visit:  # 처음 방문, cnt에 죽은 물고기 수 추가  
-                visit.append((nx, ny))
-                dfs(nx, ny, dep + 1, cnt + len(temp[nx][ny]), visit)
-                visit.pop()
-            else:  # 방문한 경우
-                dfs(nx, ny, dep + 1, cnt, visit)
+        if 0<= nx < 4 and 0<=ny <4 :
+            if (nx,ny) not in visit:
+                visit.append((nx,ny))
+                dfs(nx,ny,dep+1,cnt + len(temp[nx][ny]),visit)
+                visit.pop() # back - tracking
+            else:
+                dfs(nx,ny,dep+1,cnt,visit)
+f_dx = [0,-1,-1,-1,0,1,1,1]
+f_dy = [-1,-1,0,1,1,1,0,-1]
+dx = [-1,0,1,0]
+dy = [0,-1,0,1]
 
-#       ←, ↖,   ↑,  ↗, →, ↘, ↓, ↙
-f_dx = [0, -1, -1, -1, 0, 1, 1, 1]
-f_dy = [-1, -1, 0, 1, 1, 1, 0, -1]
-dx = [-1, 0, 1, 0]
-dy = [0, -1, 0, 1]
+M,S = map(int,input().split())
+fish =  [list(map(int,input().split())) for _ in range(M)]
+_map = [[[] for _ in range(4)] for _ in range(4)] # fish 맵과 smell 맵을 따로 
+# fish 맵의 element를 빈리스트로 쓰기
 
-m, s = map(int, input().split())
-fish = [list(map(int, input().split())) for _ in range(m)]
-graph = [[[] for _ in range(4)] for _ in range(4)]
+for x,y,d in fish:
+    _map[x-1][y-1].append(d-1)
 
-for x, y, d in fish:
-    graph[x - 1][y - 1].append(d - 1)
-
-shark = tuple(map(lambda x: int(x) - 1, input().split()))
+shark = tuple(map(lambda x: int(x)-1, input().split())) # 상어 좌표
 smell = [[0] * 4 for _ in range(4)]
 
-for _ in range(s):
-    eat = list()
+for _ in range(S):
+    eat = []
+    test = list()
     max_eat = -1
-    # 1. 모든 물고기 복제
-    temp = copy.deepcopy(graph)
+    # 1. 모든 물고기 복제하기
+    temp = copy.deepcopy(_map)
     # 2. 물고기 이동
     temp = move_fish()
-    # 3. 상어이동 - 백트래킹
-    dfs(shark[0], shark[1],0, 0, list())
-    for x, y in eat:
-        if temp[x][y]:
-            temp[x][y] = []
-            smell[x][y] = 3   # 3번 돌아야 없어짐
-    # 4. 냄새 사라짐 
-    for i in range(4):
-        for j in range(4):
-            if smell[i][j]:
-                smell[i][j] -= 1
-    # 5. 복제 마법
-    for i in range(4):
-        for j in range(4):
-            graph[i][j] += temp[i][j]
+    # 3. 상어 이동
+    dfs(shark[0],shark[1],0,0,list())
 
-# 물고기 수 구하기 
+    for x,y in eat: # 지나온 좌표들
+        if temp[x][y] :
+            temp[x][y] = [] # 물고기 제거
+            smell[x][y] = 3 # 3번 돌아야 없어짐
+    # 4. 냄새 사라짐 (아까 생성된 냄새를 2로 만들어줌과 동시에, 기존의 냄새 map에서 1씩 빼기)
+
+    for i in range(4):
+        for j in range(4):
+            if smell[i][j] :
+                smell[i][j] -= 1
+    
+    # 5 . 복제된 물고기
+    for i in range(4):
+        for j in range(4):
+            _map[i][j] += temp[i][j] # 바뀐 물고기를 더한다
+
 answer = 0
 for i in range(4):
     for j in range(4):
-        answer += len(graph[i][j])
+        answer += len(_map[i][j])
+
 
 print(answer)
