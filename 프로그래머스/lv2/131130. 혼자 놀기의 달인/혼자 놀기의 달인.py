42540bcae638c2a17216fca346dd52f189dbb3ea
Kim Seong Su <62092317+SeongSuKim95@users.noreply.github.com>
2023-01-26 06:52:52 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.19 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.py
+++ 프로그래머스/lv2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.py
@@ -1,27 +1,39 @@
-def solution(cards):
-    answer = 0
-    data = {num : index+1 for index, num in enumerate(cards)}
+# def solution(cards):
+#     answer = 0
+#     data = {num : index+1 for index, num in enumerate(cards)}
     
-    for card in cards:
-        data_ = data.copy()
-        card_ = card
-        cnt_1 = 0
-        while data_[card_]:
-            temp = data_[card_]
-            data_[card_] = 0
-            card_ = temp 
-            cnt_1 += 1
-        for card2,v in data_.items():
-            if v :
-                data_2 = data_.copy()
-                card_2 = card2
-                cnt_2 = 0
-                while data_2[card_2] :
-                    temp = data_2[card_2]
-                    data_2[card_2] = 0
-                    card_2 = temp
-                    cnt_2 += 1
-                answer = max(answer,cnt_1*cnt_2)    
+#     for card in cards:
+#         data_ = data.copy()
+#         card_ = card
+#         cnt_1 = 0
+#         while data_[card_]:
+#             temp = data_[card_]
+#             data_[card_] = 0
+#             card_ = temp 
+#             cnt_1 += 1
+#         for card2,v in data_.items():
+#             if v :
+#                 data_2 = data_.copy()
+#                 card_2 = card2
+#                 cnt_2 = 0
+#                 while data_2[card_2] :
+#                     temp = data_2[card_2]
+#                     data_2[card_2] = 0
+#                     card_2 = temp
+#                     cnt_2 += 1
+#                 answer = max(answer,cnt_1*cnt_2)    
         
     
-    return answer
+#     return answer
+
+def solution(cards):
+    answer = []
+    for i in range(len(cards)):
+        tmp = []
+        while cards[i] not in tmp:
+            tmp.append(cards[i])
+            i = cards[i] - 1
+        answer.append([] if sorted(tmp) in answer else sorted(tmp))
+    answer.sort(key=len)
+
+    return len(answer[-1]) * len(answer[-2])
