e76d0cfb1132920e97ee83b08098e88920353d3e
Kim Seong Su <62092317+SeongSuKim95@users.noreply.github.com>
2023-01-31 00:51:23 +0900

    [level 2] Title: 타겟 넘버, Time: 4.85 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,18 +1,19 @@
-cnt = 0
+from collections import deque
 
-def dfs(numbers,target,depth,sum):
-    global cnt
-    if depth == len(numbers):
+answer = 0
+def dfs(numbers,target,sum,idx):
+    global answer
+    if idx == len(numbers):
         if sum == target:
-            cnt += 1
-    else :
-        num = numbers[depth]
-        dfs(numbers,target,depth+1,sum-num)
-        dfs(numbers,target,depth+1,sum+num)
+            answer +=1
+    else:
+        cur = numbers[idx]
+        dfs(numbers,target,sum+cur,idx+1)
+        dfs(numbers,target,sum-cur,idx+1)
+        
     
 def solution(numbers,target):
+    idx = 0
+    dfs(numbers,target,0,idx)
     
-    sum = 0
-    dfs(numbers,target,0,sum)
-    
-    return cnt
+    return answer
