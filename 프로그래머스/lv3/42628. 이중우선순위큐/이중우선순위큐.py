c2c9eb0659666922790f6fd013a687579db5faf6
Kim Seong Su <62092317+SeongSuKim95@users.noreply.github.com>
2023-04-14 19:51:27 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,18 +1,26 @@
+import heapq
+
 def solution(operations):
-    answer = []
     
-    for i in operations:
-        op,num = i.split(" ")
-        if op == "I" :
-            answer.append(int(num))
-        else:
-            if answer:
-                if num == "1":
-                    answer.remove(max(answer))
-                elif num == "-1":
-                    answer.remove(min(answer))
-    if not answer:
-        answer = [0,0]
-    else:
-        answer = [max(answer),min(answer)]
-    return answer
+    min_heap, max_heap = [], []
+    for operation in operations :
+        
+        op, num = operation.split()
+        
+        if op == "I":
+            heapq.heappush(min_heap,int(num))
+            heapq.heappush(max_heap,-int(num))
+        else :
+            if num == "1": # max값 제거
+                if max_heap :
+                    max_num = -heapq.heappop(max_heap)
+                    min_heap.remove(max_num)
+            else :
+                if min_heap :
+                    min_num = heapq.heappop(min_heap)
+                    max_heap.remove(-min_num)
+    if min_heap :
+        return (-heapq.heappop(max_heap),heapq.heappop(min_heap))
+    else :
+        return [0,0]
+        
