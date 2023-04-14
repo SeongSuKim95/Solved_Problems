b701cb132f076d553a26b1830f9a4ba74b4aae9c
Kim Seong Su <62092317+SeongSuKim95@users.noreply.github.com>
2023-04-14 17:49:21 +0900

    [level 2] Title: 구명보트, Time: 8.54 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.py
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -1,22 +1,17 @@
 def solution(people, limit):
     result = 0
+    
     people.sort()
     
-    # 맨 처음 값, 마지막 값 같이 태워서
-    # limit 넘기면 마지막 값 따로 태우기
-    # 첫값은 그다음 큰값이랑 같이 태워보기
-    # 탈 수있으면 둘이 태우기
-    # list 빌때까지 반복
-    i = 0
-    j = len(people) -1
-    while i <= j:
-        
-        if people[i] + people[j] > limit:
-            result +=1
-            j -=1
-        elif people[i] + people[j] <= limit:
-            result +=1
-            i +=1
-            j -=1
+    minp, maxp = 0,len(people) - 1
     
+    while minp < maxp :
+        if people[minp] + people[maxp] > limit:
+            maxp -= 1
+        else :
+            minp += 1
+            maxp -= 1
+        result += 1
+    if minp == maxp :
+        result += 1
     return result
