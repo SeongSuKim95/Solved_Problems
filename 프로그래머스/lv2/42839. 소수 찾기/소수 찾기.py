de74084189a251043f0e86955d6cd997adea5fdf
Kim Seong Su <62092317+SeongSuKim95@users.noreply.github.com>
2023-04-17 16:12:51 +0900

    [level 2] Title: 소수 찾기, Time: 1.42 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
@@ -1,20 +1,27 @@
-from itertools import permutations as pm
-def solution(numbers):
-    answer = 0
-    data = []
-    for i in range(1,len(numbers)+1):
-        for comb in list(pm(numbers,i)):
-            temp = int(''.join(comb))
-            if temp > 1:
-                data.append(temp)
-    
-    data = list(set(data))
-    
-    for i in data:
-        for j in range(2,int(i**(1/2))+1):
-            temp = i / j
-            if temp == int(temp):
-                answer +=1
-                break
+primeSet = set()
 
-    return len(data) - answer
+def isPrime(number):
+    if number in (0, 1):
+        return False
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+
+    return True
+
+
+def makeCombinations(str1, str2):
+    if str1 != "":
+        if isPrime(int(str1)):
+            primeSet.add(int(str1))
+        # 여기서 리턴을 치지 않는다
+    for i in range(len(str2)):
+        makeCombinations(str1 + str2[i], str2[:i] + str2[i + 1:]) # 문장 combination 하는 방법
+
+
+def solution(numbers):
+    makeCombinations("", numbers)
+
+    answer = len(primeSet)
+
+    return answer
