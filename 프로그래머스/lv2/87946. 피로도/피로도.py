538c1bfaa6f44955b3bdcad45a672fc902cb2328
Kim Seong Su <62092317+SeongSuKim95@users.noreply.github.com>
2023-04-17 16:40:51 +0900

    [level 2] Title: 피로도, Time: 0.12 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.py
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -1,45 +1,25 @@
-# from itertools import permutations
+answer = -1e9
 
-# def solution(k, dungeons):
-#     answer = []
-#     # k : 유저의 현재 피로토
-#     # dungeons : ['최소 필요 피로도','소모 피로도']
+def dfs(k,cnt,dungeons,visited):
+    global answer
+    if cnt == len(dungeons) or len(dungeons) - cnt + sum(visited) <= answer:
+        answer = max(answer,sum(visited))
+        return
     
-#     comb = permutations(dungeons,len(dungeons))
-#     for i in list(comb):
-#         temp = k 
-#         cnt = 0
-#         for dungeon in i:
-#             if temp >= dungeon[0]:
-#                 temp -= dungeon[1]
-#                 cnt +=1
-#             else:
-#                 break
-#         answer.append(cnt)
-  
-#     return max(answer)
-
-## Back tracking을 이용한 풀이
-
-answer = 0
-N = 0
-visited =[]
-def dfs(k,cnt,dungeons):
+    for i in range(len(dungeons)):
+        if not visited[i] and k >= dungeons[i][0] :
+            visited[i] = True
+            dfs(k-dungeons[i][1],cnt+1,dungeons,visited)
+            visited[i] = False
+            
+            dfs(k,cnt+1,dungeons,visited)
+        
+def solution(k, dungeons):
+    
     global answer
     
-    if cnt > answer:
-        answer = cnt
-
-    for i in range(N):
-        if k >=dungeons[i][0] and not visited[i]:
-            visited[i] = 1
-            dfs(k-dungeons[i][1],cnt+1,dungeons)
-            visited[i] = 0
-
-def solution(k, dungeons):
-    global N,visited    
-    N = len(dungeons)
-    visited = [0] * N
-    dfs(k,0,dungeons)
-        
+    visited = [False] * len(dungeons)
+    
+    dfs(k,0,dungeons,visited)
+    
     return answer
