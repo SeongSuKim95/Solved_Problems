1aff89688d4d8640c4415400f5aa5743fc829298
Kim Seong Su <62092317+SeongSuKim95@users.noreply.github.com>
2023-04-17 16:45:19 +0900

    [level 2] Title: 피로도, Time: 0.08 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.py
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -1,25 +1,24 @@
-answer = -1e9
+## Back tracking을 이용한 풀이
 
-def dfs(k,cnt,dungeons,visited):
+answer = 0
+N = 0
+visited =[]
+def dfs(k,cnt,dungeons):
     global answer
-    if cnt == len(dungeons) or len(dungeons) - cnt + sum(visited) <= answer:
-        answer = max(answer,sum(visited))
-        return
     
-    for i in range(len(dungeons)):
-        if not visited[i] and k >= dungeons[i][0] :
-            visited[i] = True
-            dfs(k-dungeons[i][1],cnt+1,dungeons,visited)
-            visited[i] = False
-            
-            dfs(k,cnt+1,dungeons,visited)
-        
+    if cnt > answer:
+        answer = cnt
+
+    for i in range(N):
+        if k >=dungeons[i][0] and not visited[i]:
+            visited[i] = 1
+            dfs(k-dungeons[i][1],cnt+1,dungeons)
+            visited[i] = 0
+
 def solution(k, dungeons):
-    
-    global answer
-    
-    visited = [False] * len(dungeons)
-    
-    dfs(k,0,dungeons,visited)
-    
+    global N,visited    
+    N = len(dungeons)
+    visited = [0] * N
+    dfs(k,0,dungeons)
+        
     return answer
