f1d7964872ec2d934b4ff378ae2f98d0a9691f7b
Kim Seong Su <62092317+SeongSuKim95@users.noreply.github.com>
2023-04-17 13:07:41 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -1,23 +1,19 @@
-from collections import deque
-
 def solution(progresses, speeds):
-    
-    # 앞에 기능이 전부 100이 되어야 True
-    # 매주 몇개의 기능이 배포되는가?
     answer = []
-    q_progress = deque(progresses)
-    q_speed = deque(speeds)
-    while q_progress:
-        # 개발 진행
-        for i in range(len(q_progress)):
-            q_progress[i] += q_speed[i]
+    
+    while progresses :
+    
+        for i in range(len(progresses)):
+            progresses[i] += speeds[i]
+    
         cnt = 0
-        
-        while q_progress and q_progress[0] >= 100: # queue의 경우 앞에서부터 보면서 조건 맞으면 popleft
-            # stack의 경우 뒤에서 부터 보면서 조건 맞으면 pop
-            q_progress.popleft() # 맨 앞
-            q_speed.popleft()
-            cnt +=1
-        if cnt > 0 :
+    
+        while progresses and progresses[0] >= 100 :
+            progresses.pop(0)
+            speeds.pop(0)
+            cnt += 1
+            
+        if cnt :
             answer.append(cnt)
+    
     return answer
