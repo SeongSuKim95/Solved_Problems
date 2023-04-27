582d1a42d37326fc38232f3fda55bbfd660f43a1
Kim Seong Su <62092317+SeongSuKim95@users.noreply.github.com>
2023-04-27 16:28:51 +0900

    [Gold I] Title: 마법사 상어와 복제, Time: 1824 ms, Memory: 46416 KB -BaekjoonHub

--- 백준/Gold/23290. 마법사 상어와 복제/마법사 상어와 복제.py
+++ 백준/Gold/23290. 마법사 상어와 복제/마법사 상어와 복제.py
@@ -1,90 +1,135 @@
-import copy
+# 상어 이동 -> 상하좌우, 격자 제외
+# 물고기 이동 -> 8방향, 갈수 있을 때 까지 45도 반시계 회전, 상어/물고기 냄새/ 격자 제외
+# 8 방향 
+fish_dirs = [(0,-1),(-1,-1),(-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1)]
 
-def move_fish():
-    res = [[[] for _ in range(4)] for _ in range(4)]
+# 상어 방향  상 좌 하 우
+shark_dirs = [(-1,0),(0,-1),(1,0),(0,1)]
 
-    for row in range(4):
-        for col in range(4):
-            while temp[row][col]:
-                cur_d = temp[row][col].pop()
-                for i in range(cur_d,cur_d-8,-1): # 반시계 방향
-                    i %= 8
-                    nrow, ncol = row + f_dx[i], col + f_dy[i]
-                    if 0<= nrow < 4 and 0<= ncol <4 and (nrow,ncol) != shark and not smell[nrow][ncol]:
-                        res[nrow][ncol].append(i)
-                        break
-                else:
-                    res[row][col].append(cur_d)
-    return res
+M, S = list(map(int,input().split()))
 
-def dfs(x, y, dep, cnt, visit):
+fish_info = [list(map(lambda x : int(x)-1,input().split())) for _ in range(M)]
+shark_pos = tuple(map(lambda x : int(x)-1,input().split()))
+grid = [[[] for _ in range(4)] for _ in range(4)]
 
-    global max_eat, shark, eat
+smell_grid = [[0 for _ in range(4)] for _ in range(4)]
 
-    if dep == 3:
-        if max_eat < cnt:
-            max_eat = cnt
-            shark = (x,y)
-            eat = visit[:]
+for fish in fish_info :
+    fx, fy, fd  = fish
+    grid[fx][fy].append(fd)
+
+def print_array(array):
+    
+    for row in array:
+        print(*row)
+
+def in_range(x,y):
+    
+    return 0<=x<4 and 0<=y<4
+
+combis = []
+combi = []
+
+def choose_dir(cnt):
+    if cnt == 3:
+        combis.append(combi[:])
         return
     
-    for d in range(4):
-        nx = x + dx[d]
-        ny = y + dy[d]
-        if 0<= nx < 4 and 0<=ny <4 :
-            if (nx,ny) not in visit:
-                visit.append((nx,ny))
-                dfs(nx,ny,dep+1,cnt + len(temp[nx][ny]),visit)
-                visit.pop() # back - tracking
-            else:
-                dfs(nx,ny,dep+1,cnt,visit)
-f_dx = [0,-1,-1,-1,0,1,1,1]
-f_dy = [-1,-1,0,1,1,1,0,-1]
-dx = [-1,0,1,0]
-dy = [0,-1,0,1]
-
-M,S = map(int,input().split())
-fish =  [list(map(int,input().split())) for _ in range(M)]
-_map = [[[] for _ in range(4)] for _ in range(4)] # fish 맵과 smell 맵을 따로 
-# fish 맵의 element를 빈리스트로 쓰기
-
-for x,y,d in fish:
-    _map[x-1][y-1].append(d-1)
-
-shark = tuple(map(lambda x: int(x)-1, input().split())) # 상어 좌표
-smell = [[0] * 4 for _ in range(4)]
-
-for _ in range(S):
-    eat = []
-    test = list()
-    max_eat = -1
-    # 1. 모든 물고기 복제하기
-    temp = copy.deepcopy(_map)
-    # 2. 물고기 이동
-    temp = move_fish()
-    # 3. 상어 이동
-    dfs(shark[0],shark[1],0,0,list())
-
-    for x,y in eat: # 지나온 좌표들
-        if temp[x][y] :
-            temp[x][y] = [] # 물고기 제거
-            smell[x][y] = 3 # 3번 돌아야 없어짐
-    # 4. 냄새 사라짐 (아까 생성된 냄새를 2로 만들어줌과 동시에, 기존의 냄새 map에서 1씩 빼기)
-
     for i in range(4):
-        for j in range(4):
-            if smell[i][j] :
-                smell[i][j] -= 1
+        combi.append(i)
+        choose_dir(cnt+1)
+        combi.pop()
+
+def fish_can_go(x,y):
     
-    # 5 . 복제된 물고기
+    return in_range(x,y) and (x,y) != shark_pos and not smell_grid[x][y]
+
+def fish_move():
+    
+    new_grid = [[[] for _ in range(4)] for _ in range(4)]
+    
     for i in range(4):
         for j in range(4):
-            _map[i][j] += temp[i][j] # 바뀐 물고기를 더한다
+            if grid[i][j]:
+                for fish_dir in grid[i][j]:
+                    cur_fx, cur_fy, can_go = i,j,False
+                    for k in range(8):
+                         cur_dir = (fish_dir - k + 8) % 8
+                         nfx, nfy = cur_fx + fish_dirs[cur_dir][0], cur_fy + fish_dirs[cur_dir][1]
+                        #  print(nfx,nfy)
+                         if fish_can_go(nfx,nfy):
+                             new_grid[nfx][nfy].append(cur_dir)
+                             can_go = True
+                             break
+                    if not can_go:
+                        new_grid[i][j].append(fish_dir)
+    return new_grid
 
-answer = 0
+def shark_move():
+    
+    catched_fish = []
+    for dir_lst in combis:
+        cur_sx, cur_sy = shark_pos
+        fish_num, out_of_range = 0, False
+        grid_temp = [row[:] for row in grid]
+        # print(shark_pos)
+        for dir in dir_lst: # 3개
+            nx, ny = cur_sx + shark_dirs[dir][0], cur_sy + shark_dirs[dir][1]
+            if not in_range(nx,ny):
+                catched_fish.append(-1)
+                out_of_range = True
+                break
+            if grid_temp[nx][ny] :  
+                fish_num += len(grid_temp[nx][ny])
+                grid_temp[nx][ny] = []
+            
+            cur_sx , cur_sy = nx, ny
+        if not out_of_range : 
+            catched_fish.append(fish_num)
+    return catched_fish
+
+def change_shark_pos(lst):
+    global shark_pos
+    sx,sy = shark_pos
+    for dir in lst :
+        sx, sy = sx + shark_dirs[dir][0], sy + shark_dirs[dir][1]
+        if grid[sx][sy] : 
+            smell_grid[sx][sy] = 3
+            grid[sx][sy] = []
+    shark_pos = (sx,sy)
+
+def copy_fish(grid_temp):
+    
+    for i in range(4):
+        for j in range(4):
+            if grid_temp[i][j]:
+                
+                for fish in grid_temp[i][j]:
+                    grid[i][j].append(fish)
+    
+def smell_vanish():
+    
+    for i in range(4):
+        for j in range(4):
+            if smell_grid[i][j] :
+                smell_grid[i][j] -= 1
+
+choose_dir(0)
+# print_array(grid)
+# print("####")
+for _ in range(S):
+    grid_temp = [row[:] for row in grid]
+    grid = fish_move()
+    catched_fish = shark_move()
+    shark_way = combis[catched_fish.index(max(catched_fish))]
+    change_shark_pos(shark_way)
+    smell_vanish()
+    copy_fish(grid_temp)
+
+result = 0
 for i in range(4):
     for j in range(4):
-        answer += len(_map[i][j])
+        if grid[i][j]:
+            result += len(grid[i][j])
 
-
-print(answer)
+print(result)  
